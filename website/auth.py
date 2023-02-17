811c3a475c7b263f8c374e1b0e0ed96a9b486971
barrakz <b.rakuzy@gmail.com>
2023-02-17 23:16:24 +0100

    added basic views

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return "Login"
+
+
+@auth.route("/sign-up")
+def sign_up():
+    return "Sign Up"
+
+
+@auth.route("/logout")
+def logout():
+    return "Logout"
