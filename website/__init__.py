1e1ca50a8a669716a2394a837e25a1e2df001e4e
barrakz <b.rakuzy@gmail.com>
2023-02-21 21:13:01 +0100

    sign up login and logout work done

--- website/__init__.py
+++ website/__init__.py
@@ -29,7 +29,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        return User.query.filter.get(int(id))
+        return User.query.get(int(id))
 
     return app
 
