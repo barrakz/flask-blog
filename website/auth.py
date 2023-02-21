d8079084d31c477c6d72af48fb8fb6629b41506d
barrakz <b.rakuzy@gmail.com>
2023-02-21 17:28:52 +0100

    created database

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
     return render_template("login.html")
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
     return render_template("signup.html")
 
 
